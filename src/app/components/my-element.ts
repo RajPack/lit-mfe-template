388d3599aa1b99c9591784498501701f03638cb4
Raj <rajarajan.packianathan@siemens-healthineers.com>
2022-03-04 02:19:31 +0530

    initial commit

---
+++ src/app/components/my-element.ts
@@ -0,0 +1,14 @@
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js'
+import styles from './my-element.scss'
+console.log(styles)
+debugger
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+    static styles = [styles];
+
+    render() {
+        return html`<div class="container">App title</div>`;
+    }
+}
